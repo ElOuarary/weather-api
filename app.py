7547be03ee69a624fd2a39a8fe0cd1fc2f158d2b
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-11-16 18:54:22 +0100

    Integrating a 3rd Party Weather API

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 app = Flask(__name__)
+
+
+API_KEY = os.getenv("WEATHER_API_KEY")
+API_URL = os.getenv("WEATHER_API_URL")
+
 
 @app.route("/")
 def home():
     return "Welcome to the Weather API! Use the /weather endpoint to get weather data.", 200
 
+
 @app.route("/weather", methods=['GET'])
 def get_weather():
-    response = {
-        "city": "London",
-        "temperature": "15°C",
-        "condition": "Cloudy"
-    }
-    return jsonify(response), 200
+    city = request.args.get("city", default="London", type=str)
+    if not city:
+        return jsonify({"error": "City is required"}), 400
+    url = f"{API_URL}/{city}?unitGroup=metric&key={API_KEY}"
+    try:
+        responce = requests.get(url)
+        responce.raise_for_status()
+        data = responce.json()
+        weather_info = {
+            "city": city,
+            "temperature": data["currentConditions"]["temp"],
+            "condition": data["currentConditions"]["conditions"]
+        }
+        return jsonify(weather_info), 200
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
