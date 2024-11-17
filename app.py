07289c7760a437e7d7dc81100ca1c38f1f0c4cb5
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-11-17 12:32:31 +0100

    Adding Caching with Redis

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import os
+import redis
 import requests
 from dotenv import load_dotenv
 
@@ -8,6 +9,12 @@ load_dotenv()
 
 
 app = Flask(__name__)
+
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+CACHE_EXPIRATION = 43200  # Cache expires after 12 hours (in seconds)
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 
 API_KEY = os.getenv("WEATHER_API_KEY")
@@ -24,8 +31,13 @@ def get_weather():
     city = request.args.get("city", default="London", type=str)
     if not city:
         return jsonify({"error": "City is required"}), 400
-    url = f"{API_URL}/{city}?unitGroup=metric&key={API_KEY}"
+    # Check cache
+    cached_data = redis_client.get(city)
+    if cached_data:
+        print("Cache hit!")  # Debugging message
+        return jsonify({"city": city, "data": cached_data})
     try:
+        url = f"{API_URL}/{city}?unitGroup=metric&key={API_KEY}"
         responce = requests.get(url)
         responce.raise_for_status()
         data = responce.json()
